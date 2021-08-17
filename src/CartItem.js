ba35a660756dbac6e1e47a8c63124da7f9d693db
Vibhu Pandey <vibhupandey.vp@gmail.com>
2021-08-17 12:19:57 +0530

    added Event listener

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-	constructor(){
+	constructor() {
 		super();
-		this.state={
+		this.state = {
 			price: 999,
-			tittle:"Phone",
+			tittle: "Phone",
 			qty: 1,
 			img: ''
 		}
+		// this.increaseQuantity=this.increaseQuantity.bind(this);
+
+	}
+	increaseQuantity =()=>{
+		console.log("this",this.state)
 	}
 	render() {//jsx
-		const{price,tittle,qty}=this.state;
+		const { price, tittle, qty } = this.state;
 		return (
 			<div className="cart-item">
 				<div className="left-block">
-				<img style={styles.images}/>
+					<img style={styles.images} />
 
 				</div>
 				<div className="right-block">
-				<div style={{fontsize:25}}>{tittle}</div>
-				<div style={{color:'#777'}}>price:{price}</div>
-				<div style={{color:'#777'}}>QTY:{qty}</div>
-				<div className="cart-item-actions">
-					{/* Buttons */}
-					<img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/875/875068.png"></img>
-					<img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828906.png"></img>
-					<img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/2603/2603105.png"></img>
-				</div>
+					<div style={{ fontsize: 25 }}>{tittle}</div>
+					<div style={{ color: '#777' }}>price:{price}</div>
+					<div style={{ color: '#777' }}>QTY:{qty}</div>
+					<div className="cart-item-actions">
+						{/* Buttons */}
+						<img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/png/512/875/875068.png"></img>
+						<img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828906.png"></img>
+						<img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/2603/2603105.png"></img>
+					</div>
 
 				</div>
 			</div>
@@ -35,11 +40,11 @@ class CartItem extends React.Component {
 	}
 
 }
-const styles={
-	images:{
-		height:110,
-		width:110,
-		borderRadius:4,
+const styles = {
+	images: {
+		height: 110,
+		width: 110,
+		borderRadius: 4,
 
 	}
 }
