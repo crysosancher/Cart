e1cf8008c7513c6638159457dfe83a7f84d8e695
Crysosancher <vibhupandey.vp@gmail.com>
2021-08-19 15:14:11 +0530

    ADDed remove funtion

--- src/Cart.js
+++ src/Cart.js
@@ -37,6 +37,35 @@ class Cart extends React.Component {
 			]
 		}
 	}
+	handleIncreaseQuantity=(product)=>{
+		
+		const { products }=this.state;
+		const index=products.indexOf(product);
+		products[index].qty+=1;
+		this.setState({
+			products:products
+		},console.log('Hey Please increase the qty of the',product))
+	}
+	handleDecreaseQuantity=(product)=>{
+		
+		const { products }=this.state;
+		const index=products.indexOf(product);
+		if(products[index].qty===0){
+			return ;
+		}
+		products[index].qty-=1;
+		this.setState({
+			products:products
+		},console.log('Hey Please Decrease the qty of the',product))
+	}
+	handelDeleteProduct=(id)=>{
+		
+		const{products}=this.state;
+		const items=products.filter((item)=>item.id!==id);//Returns an array which will contain products
+		this.setState({
+		products:items
+		},console.log("Product is deleted" ,id))
+	}
 	render() {
 		const { products } = this.state;
 		return (
@@ -44,7 +73,11 @@ class Cart extends React.Component {
 				{products.map((product) => {
 					return (<CartItem 
 					product={product} 
-					key={product.id}/>
+					key={product.id}
+					onIncreaseQuantity={this.handleIncreaseQuantity}	
+					onDecreaseQuantity={this.handleDecreaseQuantity}
+					onDeleteProduct={this.handelDeleteProduct}	
+					/>
 					)
 					})}
 			</div>
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,40 +2,15 @@ import React from 'react';
 
 class CartItem extends React.Component {
 	
-		// this.increaseQuantity=this.increaseQuantity.bind(this);
-	increaseQuantity =()=>{
-		// console.log("this",this.state)
-		// this.state.qty =+1;
-		//setState form 1: not dependent on previous form
-		// this.setState({  
-		// 	qty: this.state.qty + 1
-		// });
-		//setState form 2 :dependent on previous form
-		this.setState((prevState)=>{
-			return{
-				qty:prevState.qty+1
-			}
-		},()=>{
-			console.log("this.state increaseQuantity",this.state)
-		});
-	}
-	decreaseQuantity =()=>{
-		const{qty}=this.state;//destructing of array
-		if(qty==0){
-			return;
-		}
-		//setState form 2 :dependent on previous form
-		this.setState((prevState)=>{
-			return{
-				qty:prevState.qty-1
-			}
-		},()=>{//this portion will be called when the operation of setState will be done
-			console.log("this.state decreaseQuantity",this.state)
-		});
-	}
 	render() {//jsx
 		console.log("this.props",this.props)
 		const { price, tittle, qty } = this.props.product;
+		const{
+			product,
+			onDecreaseQuantity,
+			onIncreaseQuantity,
+			onDeleteProduct
+		}=this.props;
 		return (
 			<div className="cart-item">
 				<div className="left-block">
@@ -48,11 +23,11 @@ class CartItem extends React.Component {
 					<div style={{ color: '#777' }}>QTY:{qty}</div>
 					<div className="cart-item-actions">
 						{/* Buttons */}
-						<img alt="increase" onClick={this.increaseQuantity} className="action-icons" src="https://image.flaticon.com/icons/png/512/875/875068.png"></img>
+						<img alt="increase" onClick={()=>{onIncreaseQuantity(product)}} className="action-icons" src="https://image.flaticon.com/icons/png/512/875/875068.png"></img>
 
-						<img alt="decrease" onClick={this.decreaseQuantity}  className="action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828906.png"></img>
+						<img alt="decrease" onClick={()=>{onDecreaseQuantity(product)}}className="action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828906.png"></img>
 
-						<img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/512/2603/2603105.png"></img>
+						<img alt="delete" onClick={()=>{onDeleteProduct(product.id)}} className="action-icons" src="https://image.flaticon.com/icons/png/512/2603/2603105.png"></img>
 					</div>
 
 				</div>
